802798521fa4ef85ddd207979bee054c981e2ef2
Jing <jtan1993@gmail.com>
2020-04-14 20:58:00 +0100

    init scriptable wizard

---
+++ Assets/editor/ObjectReplacer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ObjectReplacer : ScriptableWizard
+{
+    public GameObject replacementPrefab;
+    // priority to separate from other utilities under Tools
+    [MenuItem("Tools/ReplaceSelection", false, 1000)]
+    static void CreateWizard()
+    {
+        //type of wizard = ObjectReplacer class, when Ynity instantiates a new ScriptableWizard it should be of this type
+        ScriptableWizard.DisplayWizard("Object Replacer", typeof(ObjectReplacer), "Replace and close");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
