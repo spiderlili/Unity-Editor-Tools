54e0c88e24dfb30b630e09dc42f0862a0ff4515d
Jing <jtan1993@gmail.com>
2020-05-03 21:12:11 +0100

    added object visualizer

---
+++ Assets/Scripts/ExplosiveObjectVisualizer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveObjectVisualizer : MonoBehaviour
+{
+    [Range(1f, 8f)]
+    public float radiusOfExplosion = 1;
+    public float damage = 10;
+    public Color color = Color.red;
+
+    private void OnDrawGizmosSelected()
+    {
+        //display a radius wireframe
+        Gizmos.DrawWireSphere(transform.position, radiusOfExplosion);        
+    }
+}
