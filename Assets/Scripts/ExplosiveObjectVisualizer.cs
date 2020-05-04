8331006d25e214fd8402b730dba8387ae336688e
Jing <jtan1993@gmail.com>
2020-05-04 20:00:33 +0100

    added managers

--- Assets/Scripts/ExplosiveObjectVisualizer.cs
+++ Assets/Scripts/ExplosiveObjectVisualizer.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class ExplosiveObjectVisualizer : MonoBehaviour
 {
     [Range(1f, 8f)]
     public float radiusOfExplosion = 1;
     public float damage = 10;
     public Color color = Color.red;
+
+    private void OnEnable()
+    {
+        ExplosiveObjectsManager.allTheExplosives.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        ExplosiveObjectsManager.allTheExplosives.Remove(this);
+    }
 
     private void OnDrawGizmosSelected()
     {
---
+++ Assets/Scripts/ExplosiveObjectsManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveObjectsManager : MonoBehaviour
+{
+    public static List<ExplosiveObjectVisualizer> allTheExplosives = new List<ExplosiveObjectVisualizer>();
+
+    private void OnDrawGizmos()
+    {
+        foreach (ExplosiveObjectVisualizer explosive in allTheExplosives)
+        {
+            Gizmos.DrawLine(transform.position, explosive.transform.position);
+        }
+    }
+
+}
