fb6cbaa3796285723d3d0152b8586ceaabe77ecb
Jing <jtan1993@gmail.com>
2020-05-07 20:09:10 +0100

    added antialised line

--- Assets/Scripts/ExplosiveObjectsManager.cs
+++ Assets/Scripts/ExplosiveObjectsManager.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class ExplosiveObjectsManager : MonoBehaviour
 {
     public static List<ExplosiveObjectVisualizer> allTheExplosives = new List<ExplosiveObjectVisualizer>();
 
+#if UNITY_EDITOR
     private void OnDrawGizmos()
     {
         foreach (ExplosiveObjectVisualizer explosive in allTheExplosives)
         {
-            Gizmos.DrawLine(transform.position, explosive.transform.position);
+            //Gizmos.DrawLine(transform.position, explosive.transform.position);
+            Handles.DrawAAPolyLine(transform.position, explosive.transform.position); //antialised line
         }
     }
+#endif
 
 }
