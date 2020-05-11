f6ed04a6b0e16fdcbe70e4475ce47c77ccc6e3b6
Jing <jtan1993@gmail.com>
2020-05-11 23:10:01 +0100

    added asset finder

---
+++ Assets/editor/AssetFinder.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+//TODO: Search for more components
+public class AssetFinder : EditorWindow
+{
+    [MenuItem("Tools/Asset Finder/Search Project For Prefabs with Particle Systems")]
+
+    public static void FindAllPrefabsWithParticleSystems()
+    {
+        AssetFinder window = (AssetFinder)EditorWindow.GetWindow(typeof(AssetFinder));
+        window.Show();
+        window.position = new Rect(20, 80, 550, 500);
+    }
+
+    List<string> listResult;
+    List<string> filteredListResult;
+    string componentName = "ParticleSystem";
+    Vector2 scroll;
+
+    void OnGUI()
+    {
+        GUILayout.Label("Search for Prefabs with particle systems");
+        GUILayout.Space(3);
+        Rect windowRect = GUILayoutUtility.GetRect(1, 17);
+        windowRect.x += 4;
+        windowRect.width -= 7;
+
+
+        if (GUILayout.Button("Search for Prefabs with " + componentName))
+        {
+            string[] allPrefabs = EditorCommonUtilities.GetAllPrefabs();
+            listResult = new List<string>();
+            foreach (string prefab in allPrefabs)
+            {
+                UnityEngine.Object obj = AssetDatabase.LoadMainAssetAtPath(prefab);
+                GameObject go;
+                try
+                {
+                    go = (GameObject)obj;
+                    Component[] components = go.GetComponentsInChildren<ParticleSystem>(true);
+                    foreach (ParticleSystem c in components)
+                    {
+                        if (c != null)
+                        {
+                            listResult.Add(prefab);
+                        }
+                    }
+                }
+                catch
+                {
+                    Debug.Log("For some reason, prefab " + prefab + " won't cast to GameObject");
+                }
+            }
+        }
+
+        //
+        if (listResult != null)
+        {
+            filteredListResult = listResult.Distinct().ToList();
+            //Debug.Log("Filtered list is down from "+listResult +" to " + filteredListResult.Count);
+            filteredListResult.Sort();
+
+            if (filteredListResult.Count == 0)
+            {               
+                GUILayout.Label("No prefabs were found with the component type: " + componentName);
+            }
+            else
+            {
+                GUILayout.Label("The following " + filteredListResult.Count + " prefabs use the component type " + componentName);
+                
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Select All", GUILayout.Width(position.width / 4)))
+                {
+                    //TODO
+                }
+                if (GUILayout.Button("Remove " + componentName + " from All", GUILayout.Width(position.width / 3)))
+                {
+                    //TODO
+                }
+                GUILayout.EndHorizontal();
+
+                //Show results in a scroll view
+                scroll = GUILayout.BeginScrollView(scroll);
+                foreach (string str in filteredListResult)
+                {
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label(str, GUILayout.Width(position.width / 2));
+                    
+                    //Add a select button for each prefab
+                    if (GUILayout.Button("Select", GUILayout.Width(position.width / 4)))
+                    {
+                        Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(str);
+                    }
+
+                    //Add a remove button for each prefab
+                    if (GUILayout.Button("Remove " + componentName, GUILayout.Width(150)))
+                    {
+                        Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(str);
+                        GameObject activeGameObj = (GameObject)Selection.activeObject;
+                        DestroyImmediate(activeGameObj.GetComponent<ParticleSystem>(), true);
+                    }
+
+                    GUILayout.EndHorizontal();
+                }
+                
+                GUILayout.EndScrollView();
+            }
+        }
+
+    }
+}
---
+++ Assets/editor/EditorCommonUtilities.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+public static class EditorCommonUtilities
+{
+    public static string[] GetAllPrefabs()
+    {
+        string[] temp = AssetDatabase.GetAllAssetPaths();
+        List<string> result = new List<string>();
+        foreach (string s in temp)
+        {
+            if (s.Contains(".prefab")) result.Add(s);
+        }
+        return result.ToArray();
+    }
+
+    public static string GetScenePath(string sceneName, HashSet<string> scenes)
+    {
+        if (sceneName != null)
+        {
+            foreach (string scenePath in scenes)
+            {
+                if (scenePath.Contains(sceneName))
+                {
+                    return scenePath;
+                }
+            }
+        }
+        return sceneName;
+    }
+
+    public static string[] GetFullName(Transform go, float? trimSize = null)
+    {
+        string[] nameReturn = new string[2];
+        string name = go.name;
+
+        while (go.transform.parent != null)
+        {
+            go = go.parent;
+            name = string.Concat(go.name, "/", name);
+        }
+
+        nameReturn[1] = name;
+        nameReturn[0] = name;
+
+        if (trimSize != null && name.Length > trimSize)
+        {
+            nameReturn[0] = string.Concat(".../", name.Substring(name.Length - (int)trimSize));
+        }
+
+        return nameReturn;
+    }
+
+    public static bool FindTransformInScene(string original, string name)
+    {
+        if (name.Length < original.Length)
+        {
+            return false;
+        }
+        if (original.Equals(name))
+        {
+            return true;
+        }
+        else
+        {
+            int lastIndex = name.LastIndexOf("/");
+            if (lastIndex == -1)
+            {
+                return false;
+            }
+            FindTransformInScene(original, name.Substring(0, lastIndex));
+        }
+        return false;
+    }
+}
