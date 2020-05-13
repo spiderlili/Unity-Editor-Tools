3a0b7fa830f63125a6d9fc2cc88a564fbed2f21f
Jing <jtan1993@gmail.com>
2020-05-13 03:36:34 +0100

    added editor

--- Assets/editor/ExplosiveTypeEditor.cs
+++ Assets/editor/ExplosiveTypeEditor.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
+[CustomEditor(typeof(ExplosiveType))]
 public class ExplosiveTypeEditor : MonoBehaviour
 {
     // Start is called before the first frame update
