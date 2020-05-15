6b16bc3b14d1d1918c7981af5fb72afd529a227a
Jing <jtan1993@gmail.com>
2020-05-15 22:59:15 +0100

    added Inspector Fields

--- Assets/editor/ExplosiveTypeEditor.cs
+++ Assets/editor/ExplosiveTypeEditor.cs
@@ -3,27 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+//support the editing of multiple objects
+[CanEditMultipleObjects]
+
 //extend the inspector on scriptable object templates
 [CustomEditor(typeof(ExplosiveType))]
 public class ExplosiveTypeEditor : Editor
 {
+    SerializedObject so;
+    SerializedProperty propRadius;
+    SerializedProperty propDamage;
+    SerializedProperty propColor;
+
     public enum ExplosiveObject 
     { 
         barrel,
         doll
     };
 
+    private void OnEnable()
+    {
+        so = serializedObject;
+        propRadius = so.FindProperty("radiusOfExplosion");
+        propDamage = so.FindProperty("damage");
+        propColor = so.FindProperty("meshColor");
+    }
+
     ExplosiveObject explosiveObjects;
     public override void OnInspectorGUI()
     {
-        DrawDefaultInspector();
-
-        using(new GUILayout.HorizontalScope())
-        {
-            GUILayout.Label("Explosive Object");
-            explosiveObjects = (ExplosiveObject)EditorGUILayout.EnumPopup(explosiveObjects);
-        }
-
-        EditorGUILayout.ObjectField("Assign transform: ", null, typeof(Transform), true);
+        so.Update();
+        EditorGUILayout.PropertyField(propRadius);
+        EditorGUILayout.PropertyField(propDamage);
+        EditorGUILayout.PropertyField(propColor);
+        so.ApplyModifiedProperties();
     }
 }
