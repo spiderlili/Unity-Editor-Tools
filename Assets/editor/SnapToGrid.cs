6f8257e108d87ae848566927addd3c616ccad138
Jing <jtan1993@gmail.com>
2020-05-19 15:18:40 +0100

    added validation

--- Assets/editor/SnapToGrid.cs
+++ Assets/editor/SnapToGrid.cs
@@ -5,12 +5,21 @@ using UnityEditor;
 
 public static class SnapToGrid 
 {
-    [MenuItem("Edit/Snap Selected Object To Grid")]
+    const string UNDO_STR_SNAP = "Snap Objects";
+
+    [MenuItem("Edit/Snap Selected Object To Grid %&S", isValidateFunction: true)] //disabled if nothing is selected
+    public static bool CanSnapThingsValidate()
+    {
+        return Selection.gameObjects.Length > 0;
+    }
+
+    [MenuItem("Edit/Snap Selected Object To Grid %&S")]
     public static void SnapThings()
     {
         foreach(GameObject selectedObj in Selection.gameObjects)
         {
             selectedObj.transform.position = selectedObj.transform.position.Round();
+            Undo.RecordObject(selectedObj.transform, UNDO_STR_SNAP);
             //Debug.Log("snapped");
         }
     }
@@ -22,4 +31,6 @@ public static class SnapToGrid
         v.z = Mathf.Round(v.z);
         return v;
     }
+
+    //TODO: Display message if already snapped
 }
