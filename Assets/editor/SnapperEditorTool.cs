b2e790577c6f04d404a369e30ecbd4c7621c8993
Jing <jtan1993@gmail.com>
2020-05-20 16:40:54 +0100

    added advanced snapper

---
+++ Assets/editor/SnapperEditorTool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SnapperEditorTool : EditorWindow
+{
+    [MenuItem("Tools/Advanced Snapper")]
+    public static void OpenTool() => GetWindow<SnapperEditorTool>("Advanced Snapper");
+    private void OnEnable()
+    {
+        Selection.selectionChanged += Repaint;
+    }
+
+    private void OnDisable()
+    {
+        Selection.selectionChanged -= Repaint;
+    }
+
+    private void OnGUI()
+    {
+        using(new EditorGUI.DisabledScope(Selection.gameObjects.Length == 0))
+        {
+            if(GUILayout.Button("Snap Selection"))
+            {
+                SnapSelection();
+            }
+        }
+    }
+
+    void SnapSelection()
+    {
+        foreach (GameObject selectedObj in Selection.gameObjects)
+        {
+            Undo.RecordObject(selectedObj.transform, "Snap objects");
+            selectedObj.transform.position = selectedObj.transform.position.Round();
+            //Debug.Log("snapped");
+        }
+    }
+}
