763f8628aa6cc9f19e1c9de0eb779edda4b2bf57
Jing <jtan1993@gmail.com>
2020-05-26 18:04:38 +0100

    added comments

---
+++ Assets/Scripts/Utilities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+
+// This component is auto-erased at building time on this class EraseComponentsAfterSceneBuild.
+public class Comment : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [Serializable]
+    public enum CommentStates
+    {
+        Edit,
+        Info,
+        Alert,
+        Error
+    }
+
+    public CommentStates State;
+    public string Text;
+#endif
+}
---
+++ Assets/editor/CommentEditor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Comment))]
+public class CommentEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Comment commentScript = (Comment)target;
+
+        // Space at the beginning
+        EditorGUILayout.Space();
+        EditorGUI.BeginChangeCheck();
+
+        // Select the mode
+        commentScript.State = (Comment.CommentStates)EditorGUILayout.EnumPopup(commentScript.State, GUILayout.Width(50));
+
+        // Show the corresponding help box depending on the state we are
+        EditorGUILayout.Space();
+        if (commentScript.State == Comment.CommentStates.Edit)
+        {
+            EditorStyles.textArea.wordWrap = true;
+            commentScript.Text = EditorGUILayout.TextArea(commentScript.Text, EditorStyles.textArea);
+        }
+        else if (commentScript.State == Comment.CommentStates.Info)
+        {
+            EditorGUILayout.HelpBox(commentScript.Text, MessageType.Info);
+        }
+        else if (commentScript.State == Comment.CommentStates.Alert)
+        {
+            EditorGUILayout.HelpBox(commentScript.Text, MessageType.Warning);
+        }
+        else if (commentScript.State == Comment.CommentStates.Error)
+        {
+            EditorGUILayout.HelpBox(commentScript.Text, MessageType.Error);
+        }
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            EditorUtility.SetDirty(commentScript);
+        }
+
+        EditorGUILayout.Space();
+    }
+}
