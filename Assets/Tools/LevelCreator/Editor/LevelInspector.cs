92d3e9ad45dcdc676318e91ad3898d81b48f83c6
Jing <jtan1993@gmail.com>
2020-06-21 23:55:06 +0100

    added sprite

--- Assets/Tools/LevelCreator/Editor/LevelInspector.cs
+++ Assets/Tools/LevelCreator/Editor/LevelInspector.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 
 namespace LevelCreator 
 { 
-    [CustomEditor(typeof(Level))]
+    [CustomEditor(typeof(Level))] //overwrite the inspector of all the Level class instances
     public class LevelInspector : Editor
     {
-        private Level _myTargetLevel;
+        private Level _TargetLevel;
+        [Header("Visualization")]
+        public int varVizTest;
 
         //called every time the inspected object is selected & after script is loaded: all the init code goes here
         private void OnEnable() 
         {
             //target has a reference to the object inspected: used to access properties of that, manipulate them in custom inspector
-            _myTargetLevel = (Level)target; //use targets to support multi-object editing (returns object array)
+            _TargetLevel = (Level)target; //use targets to support multi-object editing (returns object array)
         }
 
         //called when the inspected object goes out of scope & when the object is destroyed, all the cleanup code goes here
@@ -33,13 +36,14 @@ namespace LevelCreator
         {
             base.OnInspectorGUI();
             EditorGUILayout.LabelField("The GUI of this inspector was modified.");
-            DrawDefaultInspector();
+            //DrawDefaultInspector();
             DrawLevelDataGUI();
         }
 
         private void DrawLevelDataGUI()
         {
-            //EditorGUI.LabelField("Data", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Data", EditorStyles.boldLabel);
+            _TargetLevel.Background = (Sprite)EditorGUILayout.ObjectField("Background", _TargetLevel.Background, typeof(Sprite), false);
         }
     }
 }
--- Assets/Tools/LevelCreator/Scripts/Level.cs
+++ Assets/Tools/LevelCreator/Scripts/Level.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Sprite Background;
     void Start()
     {
         
