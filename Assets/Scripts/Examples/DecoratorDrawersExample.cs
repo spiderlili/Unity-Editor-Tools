fc800e60985a5118119ab9e2017a8deaf1b11e05
Jing <jtan1993@gmail.com>
2020-07-03 08:29:33 +0100

    added decorator drawers

---
+++ Assets/Scripts/Examples/DecoratorDrawersExample.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//decorator drawers are designed to draw decoration in the inspector - unassociated with a specific field
+public class DecoratorDrawersExample : MonoBehaviour
+{
+    [Header("Group of variables")]
+    public int varA = 10;
+    public int varB = 20;
+
+    [Space(40)] 
+    public int spacedOutVarD = 20;
+
+    [Tooltip("This is a tooltip")]
+    public int ReadToolTip = 30;
+}
