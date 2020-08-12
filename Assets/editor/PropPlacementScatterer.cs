239d4859a8b6db160eb9e8bb14faa25f3f69d554
Jing <jtan1993@gmail.com>
2020-08-12 07:39:48 +0100

    added event

--- Assets/editor/PropPlacementScatterer.cs
+++ Assets/editor/PropPlacementScatterer.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class PropPlacementScatterer : MonoBehaviour
+public class PropPlacementScatterer : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    [MenuItem("Tools/Prop Placement Scatterer")]
+    public static void OpenPropPlacermentTool() => GetWindow<PropPlacementScatterer>();
+
+    public float radius = 2f;
+    public int spawnCount = 8;
+
+    private void OnEnable() 
     {
+        //sign up to an event called in every scene's onGUI event when the window is opened
+        SceneView.duringSceneGui += DuringSceneGUI;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DuringSceneGUI(SceneView) //called per scene view you have open: can have multiple scenes open
     {
-        
+        //scene view raycasting: assumes camera centre = where to place things
+    }
+
+    private void OnDisable()
+    {
+        SceneView.duringSceneGui -= DuringSceneGUI; //unsubscribe from event
     }
 }
