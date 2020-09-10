0a7a409e4808b29b9c79569015d6ffbcf4314b80
Jing <jtan1993@gmail.com>
2020-09-10 07:43:09 +0100

    added enemy designer

---
+++ Assets/editor/EnemyDesignerWindow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyDesignerWindow : EditorWindow
+{
+    [MenuItem("Tools/Enemy Designer")]
+    static void OpenWindow()
+    {
+        EnemyDesignerWindow window = (EnemyDesignerWindow) GetWindow(typeof(EnemyDesignerWindow));
+        window.minSize = new Vector2(600, 300); //make sure the window is not too small
+    }
+}
