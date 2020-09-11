fc7c398df6f7de40d1119996fa4a3ba259525645
Jing <jtan1993@gmail.com>
2020-09-11 11:34:34 +0100

    funcs

--- Assets/editor/EnemyDesignerWindow.cs
+++ Assets/editor/EnemyDesignerWindow.cs
@@ -10,5 +10,46 @@ public class EnemyDesignerWindow : EditorWindow
     {
         EnemyDesignerWindow window = (EnemyDesignerWindow) GetWindow(typeof(EnemyDesignerWindow));
         window.minSize = new Vector2(600, 300); //make sure the window is not too small
+        window.Show();
+    }
+
+    private void OnEnable() //similar to start() in game scripting
+    {
+
+    }
+
+    void InitTextures() //init Texture2D values
+    {
+
+    }
+
+    private void OnGUI() //similar to update() in game scripting BUT called >=1 times per interaction - anytime mouse goes over the window/interaction
+    {
+
+    }
+
+    void DrawLayouts() //called inside onGUI: Define rect values & paints textures based on rects
+    {
+
+    }
+
+    void DrawHeader()
+    {
+
+    }
+
+    void DrawMageSettings()
+    {
+
+    }
+
+    void DrawWarriorSettings()
+    {
+
+    }
+
+    void DrawRogueSettings()
+    {
+
     }
 }
