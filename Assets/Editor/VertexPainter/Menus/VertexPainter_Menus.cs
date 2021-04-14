09024c18eb9c24e99426627ace0ddedb582feb72
Jing Chun Tan <jtan1993@gmail.com>
2021-04-14 21:40:29 +0100

    vertex painter init

---
+++ Assets/Editor/VertexPainter/Menus/VertexPainter_Menus.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class VertexPainter_Menus : MonoBehaviour
+{
+    [MenuItem("Tools/Art/Vertex Painter", false)]
+    static void LaunchVertexPainter()
+    {
+        VertexPainter_Window.LaunchVertexPainter();
+    }
+}
---
+++ Assets/Editor/VertexPainter/Windows/VertexPainter_Window.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEditor;
+public class VertexPainter_Window : EditorWindow
+{
+    #region Variables
+    #endregion
+
+    #region MainMethod
+    public static void LaunchVertexPainter() //init the editor window: normal window, title, focused on launch
+    {
+        var vertexPainterWindow = EditorWindow.GetWindow<VertexPainter_Window>(false, "Vertex Painter", true);
+    }
+
+    #endregion
+
+    #region GUIMethods
+    #endregion
+
+    #region UtilityMethods
+    #endregion
+}
