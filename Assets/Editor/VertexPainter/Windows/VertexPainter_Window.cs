39000a4e1ceb9284ffca46d2a987576694f00d5b
Jing Chun Tan <jtan1993@gmail.com>
2021-06-12 11:59:07 +0100

    styled window

--- Assets/Editor/VertexPainter/Windows/VertexPainter_Window.cs
+++ Assets/Editor/VertexPainter/Windows/VertexPainter_Window.cs
@@ -20,37 +20,39 @@ public class VertexPainter_Window : EditorWindow
     {
         //header
         EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField("Vertex Painter", EditorStyles.boldLabel);
+        //EditorGUILayout.LabelField("Vertex Painter", EditorStyles.boldLabel);
+        GUILayout.Box("Vertex Painter", GUILayout.Height(60), GUILayout.ExpandWidth(true));
         EditorGUILayout.EndHorizontal();
 
-        //body
-        EditorGUILayout.BeginVertical();
+        //body: styled using default box GUI skin to create box layout
+        EditorGUILayout.BeginVertical(GUI.skin.box);
         EditorGUILayout.Space(10);
 
         //50/50 divide horizontally for 2 buttons
         EditorGUILayout.BeginHorizontal();
-        if (GUILayout.Button("Button1"))
+        if (GUILayout.Button("Button1", GUILayout.Height(30)))
         {
 
         }
 
-        if (GUILayout.Button("Button2"))
+        if (GUILayout.Button("Button2", GUILayout.Height(30)))
         {
 
         }
         EditorGUILayout.EndHorizontal();
 
         //100% width for 3rd button
-        if (GUILayout.Button("Button3"))
+        if (GUILayout.Button("Button3", GUILayout.Height(30)))
         {
 
         }
 
-        GUILayout.FlexibleSpace();
+        GUILayout.FlexibleSpace(); //make the content after FlexibleSpace() always stay at the bottom - great for footer
         EditorGUILayout.EndVertical();
 
         //footer
-        EditorGUILayout.LabelField("Title");
+        //EditorGUILayout.LabelField("Title");
+        GUILayout.Box("Title", GUILayout.Height(60), GUILayout.ExpandWidth(true));
 
         //update & repaint the UI in real time
         Repaint();
