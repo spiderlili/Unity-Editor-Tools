dd66ed16c7dae06f977139cd9d40e693cf086e8a
Jing Chun Tan <jtan1993@gmail.com>
2021-06-13 14:41:33 +0100

    added textures

--- Assets/Editor/VertexPainter/Windows/VertexPainter_Window.cs
+++ Assets/Editor/VertexPainter/Windows/VertexPainter_Window.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class VertexPainter_Window : EditorWindow
 {
     #region Variables
+    GUIStyle boxStyle;
     #endregion
 
     #region MainMethod
     public static void LaunchVertexPainter() //init the editor window: normal window, title, focused on launch
     {
         var vertexPainterWindow = EditorWindow.GetWindow<VertexPainter_Window>(false, "Vertex Painter", true);
+        vertexPainterWindow.GenerateStyles();
     }
 
     #endregion
@@ -21,7 +23,7 @@ public class VertexPainter_Window : EditorWindow
         //header
         EditorGUILayout.BeginHorizontal();
         //EditorGUILayout.LabelField("Vertex Painter", EditorStyles.boldLabel);
-        GUILayout.Box("Vertex Painter", GUILayout.Height(60), GUILayout.ExpandWidth(true));
+        GUILayout.Box("Vertex Painter", boxStyle, GUILayout.Height(60), GUILayout.ExpandWidth(true));
         EditorGUILayout.EndHorizontal();
 
         //body: styled using default box GUI skin to create box layout
@@ -42,9 +44,9 @@ public class VertexPainter_Window : EditorWindow
         EditorGUILayout.EndHorizontal();
 
         //100% width for 3rd button
-        if (GUILayout.Button("Button3", GUILayout.Height(30)))
+        if (GUILayout.Button("Update Styles", GUILayout.Height(30)))
         {
-
+            GenerateStyles();
         }
 
         GUILayout.FlexibleSpace(); //make the content after FlexibleSpace() always stay at the bottom - great for footer
@@ -60,5 +62,11 @@ public class VertexPainter_Window : EditorWindow
     #endregion
 
     #region UtilityMethods
+    private void GenerateStyles()
+    {
+        boxStyle = new GUIStyle();
+        boxStyle.margin = new RectOffset(100, 10, 10, 10);
+    }
+
     #endregion
 }
