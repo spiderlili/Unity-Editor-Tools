642f21f14674e718ac921a705d903102b7fb5462
Jing Chun Tan <jtan1993@gmail.com>
2021-06-15 12:52:41 +0100

    updated user events

--- Assets/Editor/VertexPainter/Windows/VertexPainter_Window.cs
+++ Assets/Editor/VertexPainter/Windows/VertexPainter_Window.cs
@@ -6,6 +6,7 @@ public class VertexPainter_Window : EditorWindow
 {
     #region Variables
     GUIStyle boxStyle;
+    public Vector2 mousePos = Vector2.zero;
     #endregion
 
     #region MainMethod
@@ -87,64 +88,40 @@ public class VertexPainter_Window : EditorWindow
         Handles.color = new Color(1.0f, 1.0f, 1.0f, 1.0f); ;
         Handles.DrawWireDisc(Vector3.zero, Vector3.up, 2f);
         Handles.DrawSolidDisc(Vector3.zero, Vector3.up, 0.5f);
-        // Event.current houses information on scene view input this cycle
-        Event current = Event.current;
 
-        // If user has pressed the Left Mouse Button, do something and
-        // swallow it so nothing else hears the event
-        if (current.type == EventType.MouseDown && current.button == 0)
-        {
-            // While this tool is open, only allow the user to select scene
-            // objects with a Collider component on them
-            if (!Select<Collider>(current))
-            {
-                // If nothing with Collider found, unselect everything
-                Selection.activeGameObject = null;
-            }
-        }
-
-        // After you've done all your custom event interpreting and swallowing,
-        // you have to call this code to make sure swallowed events don't bleed out.
-        // Not sure why, but that's the rules.
-        if (current.type == EventType.Layout)
-            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(GetHashCode(), FocusType.Passive));
-    }
-
-    // When user attempts to select an object, this sees if they selected an object with the given component. 
-    // This will swallow the event and select the object if successful.
-    /// <param name="e">Event from OnSceneGUI</param>
-    /// <typeparam name="T">Component type</typeparam>
-    /// <returns>Returns the object</returns>
-    public static GameObject Select<T>(Event e) where T : UnityEngine.Component
-    {
-        Camera cam = Camera.current;
-
-        if (cam != null)
-        {
-            RaycastHit hit;
-            Ray ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.collider != null)
-                {
-                    GameObject gameObj = hit.collider.gameObject;
-                    if (gameObj.GetComponent<T>() != null)
-                    {
-                        e.Use();
-                        UnityEditor.Selection.activeGameObject = gameObj;
-                        return gameObj;
-                    }
-                }
-            }
-        }
-
-        return null;
+        //Get user inputs
+        ProcessInputs();
+        //        Debug.Log(mousePos);
+        sceneView.Repaint(); //update & repaint sceneView GUI
     }
 
     #endregion
 
     #region UtilityMethods
+    private void ProcessInputs() //controlled via OnSceneGUI()
+    {
+        // Event.current houses information on scene view input this cycle being processed right now
+        Event currentEvt = Event.current;
+
+        mousePos = currentEvt.mousePosition;
+
+        if (currentEvt.type == EventType.KeyDown)
+        {
+            if (currentEvt.isKey && currentEvt.keyCode == KeyCode.T)
+            {
+                Debug.Log("Pressed the T Key");
+            }
+        }
+
+        if (currentEvt.type == EventType.MouseDown)
+        {
+            if (currentEvt.button == 0)
+            {
+                Debug.Log("Pressed the Left Mouse Button");
+            }
+        }
+    }
+
     private void GenerateStyles()
     {
         boxStyle = new GUIStyle();
