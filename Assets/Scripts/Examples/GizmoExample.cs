a3e305610baa310a232de4e8ad492ecc8178621f
Jing <jtan1993@gmail.com>
2021-06-26 17:00:24 +0100

    gizmo example

---
+++ Assets/Scripts/Examples/GizmoExample.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+//goal: implement the OnDrawGizmos and OnDrawGizmosSelected methods to achieve a similar but more flexible solution to adding simple gizmos in Unity
+public class GizmoExample : MonoBehaviour
+{
+    //The OnDrawGizmos method is not called if the component collapses in the inspector window.
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawCube(transform.position, Vector3.one);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, Vector3.one);
+    }
+}
