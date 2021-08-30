516b46315717ebfce06bc55132bb14d13e034170
Jing Tan <jtan1993@gmail.com>
2021-08-30 17:43:20 +0100

    init strip shader variants

---
+++ Assets/Editor/StripShaderVariants.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class StripShaderVariants : IPreprocessShaders
+{
+    // Returns the relative callback order for callbacks. Callbacks with lower values are called before ones with higher values.
+    // Useful if you have several scripts to determine which one has priority
+    public int callbackOrder { get; }
+}
