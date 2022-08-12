5b41afa475741509939ba381e9435de57d62f867
Jing Tan <jtan1993@gmail.com>
2022-08-12 12:39:53 +0100

    Update TimeDrawer.cs

--- Assets/Scripts/Examples/Editor/TimeDrawer.cs
+++ Assets/Scripts/Examples/Editor/TimeDrawer.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class TimeDrawer : MonoBehaviour
+[CustomPropertyDrawer(typeof(TimeAttribute))]
+public class TimeDrawer : PropertyDrawer
 {
-    // Start is called before the first frame update
-    void Start()
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        
+        return EditorGUI.GetPropertyHeight(property) * 2;
+    }
+    /*
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label);
+    {
+        if (property.propertyType == SerializedPropertyType.Integer) {
+            property.intValue = EditorGUI.IntField(new Rect(position.x, position.y, position.width, position.height / 2), label, Mathf.Max(0, property.intValue));
+            EditorGUI.LabelField (new Rect (position.x, position.y + position.height / 2, position.width, position.height / 2), " ",
+                TimeFormat (property.intValue));
+        } else {
+            EditorGUI.HelpBox (position, "To use the Time attribute \"" + label.text + "\" must be an int!", MessageType.Error);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private string TimeFormat(int totalSeconds)
     {
+        TimeAttribute time = attribute as TimeAttribute;
         
     }
+    */
 }
