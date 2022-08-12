bcd8b24b8d316c9290902e712fc46c124de96d83
Jing Tan <jtan1993@gmail.com>
2022-08-12 12:31:48 +0100

    Update TimeAttribute.cs

--- Assets/Scripts/Examples/TimeAttribute.cs
+++ Assets/Scripts/Examples/TimeAttribute.cs
@@ -2,7 +2,7 @@
 
 public class TimeAttribute : PropertyAttribute
 {
-    public readonly bool DisplayHours;
+    public readonly bool DisplayHours; // Optional parameter
     public TimeAttribute(bool displayHours = false) //default: Time attribute will display in m:s format
     {
         DisplayHours = displayHours; //if true: display time in h:m:s format
