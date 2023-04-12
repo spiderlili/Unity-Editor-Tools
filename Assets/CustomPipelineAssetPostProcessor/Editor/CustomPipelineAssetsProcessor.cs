60dc40723aa9b65e4f1328845b8505bc40238422
Jing Tan <jtan1993@gmail.com>
2023-04-12 16:13:03 +0100

    set global scale

--- Assets/CustomPipelineAssetPostProcessor/Editor/CustomPipelineAssetsProcessor.cs
+++ Assets/CustomPipelineAssetPostProcessor/Editor/CustomPipelineAssetsProcessor.cs
@@ -19,6 +19,8 @@ public class CustomPipelineAssetsProcessor : AssetPostprocessor
     private void OnPreprocessModel()
     {
         ModelImporter modelImporter = (ModelImporter)assetImporter;
+        modelImporter.globalScale = 1f;
+
     }
 
 #endregion
