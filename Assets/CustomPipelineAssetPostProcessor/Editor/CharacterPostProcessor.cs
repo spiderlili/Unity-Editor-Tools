5f3e8dcacdfc6652d0d9899304d4a4f9b8b806b0
Jing Tan <jtan1993@gmail.com>
2023-04-13 17:51:44 +0100

    update char processor

--- Assets/CustomPipelineAssetPostProcessor/Editor/CharacterPostProcessor.cs
+++ Assets/CustomPipelineAssetPostProcessor/Editor/CharacterPostProcessor.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEditor;
 
-public class CharacterPostProcessor : MonoBehaviour
+public class CharacterPostProcessor : AssetPostprocessor
 {
-    // Start is called before the first frame update
-    void Start()
+#region Paths
+    
+    private const string ProjectCharactersPath = "Assets/Models/Characters/"; // Special settings for normal maps
+    
+#endregion
+    
+#region ModelImporters
+
+    private void OnPreprocessModel()
     {
+        if (!assetPath.Contains(ProjectCharactersPath)) {
+            return;
+        }
+        
+        ModelImporter modelImporter = (ModelImporter)assetImporter;
+        // modelImporter.globalScale = 1f; // Set in DCC
+        modelImporter.useFileScale = true;
+        modelImporter.importBlendShapes = false;
+        modelImporter.importVisibility = false;
+        modelImporter.importCameras = false;
+        modelImporter.importLights = false;
+        modelImporter.preserveHierarchy = false;
+        modelImporter.sortHierarchyByName = false;
+        modelImporter.meshCompression = ModelImporterMeshCompression.Medium;
+        modelImporter.isReadable = false;
+        modelImporter.optimizeMeshPolygons = true; // Optimise polygon order
+        modelImporter.optimizeMeshVertices = true; // Optimise vertex order
+        modelImporter.addCollider = false;
+        modelImporter.keepQuads = false;
+        modelImporter.weldVertices = true;
+        modelImporter.indexFormat = ModelImporterIndexFormat.Auto;
+        modelImporter.importNormals = ModelImporterNormals.Import;
+        modelImporter.importBlendShapeNormals = modelImporter.importNormals;
+        modelImporter.normalCalculationMode = ModelImporterNormalCalculationMode.AreaAndAngleWeighted;
+        modelImporter.normalSmoothingSource = ModelImporterNormalSmoothingSource.PreferSmoothingGroups;
+        modelImporter.normalSmoothingAngle = 60;
+        modelImporter.importTangents = ModelImporterTangents.CalculateMikk;
+        modelImporter.swapUVChannels = false;
+        modelImporter.generateSecondaryUV = false;
+
+        modelImporter.animationType = ModelImporterAnimationType.Generic;
+        modelImporter.avatarSetup = ModelImporterAvatarSetup.NoAvatar;
+        modelImporter.skinWeights = ModelImporterSkinWeights.Standard;
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+#endregion
 }
--- Assets/CustomPipelineAssetPostProcessor/Editor/CustomPipelineAssetsProcessor.cs
+++ Assets/CustomPipelineAssetPostProcessor/Editor/CustomPipelineAssetsProcessor.cs
@@ -16,33 +16,35 @@ public class CustomPipelineAssetsProcessor : AssetPostprocessor
 
     private void OnPreprocessModel()
     {
-        if (assetPath.Contains(ProjectEnvironmentsPath)) {
-            ModelImporter modelImporter = (ModelImporter)assetImporter;
-            modelImporter.globalScale = 1f;
-            modelImporter.useFileScale = true;
-            modelImporter.importBlendShapes = false;
-            modelImporter.importVisibility = false;
-            modelImporter.importCameras = false;
-            modelImporter.importLights = false;
-            modelImporter.preserveHierarchy = false;
-            modelImporter.sortHierarchyByName = false;
-            modelImporter.meshCompression = ModelImporterMeshCompression.Medium;
-            modelImporter.isReadable = false;
-            modelImporter.optimizeMeshPolygons = true; // Optimise polygon order
-            modelImporter.optimizeMeshVertices = true; // Optimise vertex order
-            modelImporter.addCollider = false;
-            modelImporter.keepQuads = false;
-            modelImporter.weldVertices = true;
-            modelImporter.indexFormat = ModelImporterIndexFormat.Auto;
-            modelImporter.importNormals = ModelImporterNormals.Import;
-            modelImporter.importBlendShapeNormals = modelImporter.importNormals;
-            modelImporter.normalCalculationMode = ModelImporterNormalCalculationMode.AreaAndAngleWeighted;
-            modelImporter.normalSmoothingSource = ModelImporterNormalSmoothingSource.PreferSmoothingGroups;
-            modelImporter.normalSmoothingAngle = 60;
-            modelImporter.importTangents = ModelImporterTangents.None;
-            modelImporter.swapUVChannels = false;
-            modelImporter.generateSecondaryUV = false;
+        if (!assetPath.Contains(ProjectEnvironmentsPath)) {
+            return;
         }
+        
+        ModelImporter modelImporter = (ModelImporter)assetImporter;
+        // modelImporter.globalScale = 1f; // Set in DCC
+        modelImporter.useFileScale = true;
+        modelImporter.importBlendShapes = false;
+        modelImporter.importVisibility = false;
+        modelImporter.importCameras = false;
+        modelImporter.importLights = false;
+        modelImporter.preserveHierarchy = false;
+        modelImporter.sortHierarchyByName = false;
+        modelImporter.meshCompression = ModelImporterMeshCompression.Medium;
+        modelImporter.isReadable = false;
+        modelImporter.optimizeMeshPolygons = true; // Optimise polygon order
+        modelImporter.optimizeMeshVertices = true; // Optimise vertex order
+        modelImporter.addCollider = false;
+        modelImporter.keepQuads = false;
+        modelImporter.weldVertices = true;
+        modelImporter.indexFormat = ModelImporterIndexFormat.Auto;
+        modelImporter.importNormals = ModelImporterNormals.Import;
+        modelImporter.importBlendShapeNormals = modelImporter.importNormals;
+        modelImporter.normalCalculationMode = ModelImporterNormalCalculationMode.AreaAndAngleWeighted;
+        modelImporter.normalSmoothingSource = ModelImporterNormalSmoothingSource.PreferSmoothingGroups;
+        modelImporter.normalSmoothingAngle = 60;
+        modelImporter.importTangents = ModelImporterTangents.None;
+        modelImporter.swapUVChannels = false;
+        modelImporter.generateSecondaryUV = false;
     }
 
 #endregion
