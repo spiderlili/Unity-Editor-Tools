995544895cd0cc9ec7b2864a3ed6916ec51ff2d7
Jing Tan <jtan1993@gmail.com>
2023-06-14 22:05:00 +0100

    add screenshot exporter

---
+++ Assets/ScreenshotExporter/Editor/ScreenshotExporterEditorWindow.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class ScreenshotExporterEditorWindow : EditorWindow
+{
+    private string fileName;
+    private string filePath;
+    private static readonly string DefaultFileName = "Screenshot";
+    private static readonly string DefaultFileDirectory = "Assets/";
+    
+    [MenuItem("Tools/Screenshot Exporter")]
+    static void ShowWindow()
+    {
+        var window = GetWindow<ScreenshotExporterEditorWindow>();
+        window.titleContent = new GUIContent("Screenshot Exporter");
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        fileName = EditorGUILayout.TextField("Screenshot File Name: ", DefaultFileName);
+        
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PrefixLabel("Save to Folder: ");
+        if (GUILayout.Button("...")) {
+            filePath = EditorUtility.OpenFilePanel("Choose Folder", DefaultFileDirectory, "");
+        }
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+
+        if (!GUILayout.Button("Take Screenshot")) {
+            string path = $"{filePath}/{fileName}.png";
+            ScreenCapture.CaptureScreenshot(path);
+        }
+        
+    }
+}
